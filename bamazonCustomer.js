12742a6eda71a6692272d5abf2d420cffefdb58e
RJ <rj@blacklambs.com>
2017-09-25 11:45:15 -0400

    tweak

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,6 +47,7 @@ function start() {
                 break;
 
             case "Buy an item":
+                products();
                 buyItem();
                 break;
 
@@ -62,7 +63,8 @@ function products() {
         for (var i = 0; i < response.length; i++) {
             console.log(response[i].item_id + ". " + response[i].product_name + " - $" + response[i].price + " - Quantity Remaining: " + response[i].stock_quantity)
         }
-        start();
+        // start();
+        // buyItem();
 
     })
 
@@ -71,57 +73,53 @@ function products() {
 
 function buyItem() {
     connection.query("SELECT * FROM products", function(error, response) {
-        
-        inquirer.prompt([
-    {
-      type: "input",
-      name: "itemID",
-      message: "What is the ID of the product you would like to purchase?",
-      validate: function(value){
-        if(isNaN(value) == false && parseInt(value) <= response.length && parseInt(value) > 0){
-          return true;
-        } else{
-          return false;
-        }
-      }
-    },
-    {
-      type: "input",
-      name: "quantity",
-      message: "What quantity would you like to purchase?",
-      validate: function(value){
-        if(isNaN(value)){
-          return false;
-        } else{
-          return true;
-        }
-      }
-    }
-    ]).then(function(answer) {
 
-            var purchasedItem = (answer.itemID) - 1;
-            var numberToBuy = parseInt(answer.quantity);
-            var purchaseTotal = parseFloat(((response[purchasedItem].Price) * numberToBuy).toFixed(2));
-
-            //check if quantity is sufficient
-            if (response[purchasedItem].StockQuantity >= numberToBuy) {
-                //after purchase, updates quantity in Products
-                connection.query("UPDATE Products SET ? WHERE ?", [{
-                    StockQuantity: (response[purchasedItem].StockQuantity - numberToBuy)
-                }, {
-                    ItemID: answer.itemID
-                }], function(err, result) {
-                    if (err) throw err;
-                    console.log("Success! Your total is $" + purchaseTotal.toFixed(2) + ". Thanks for your business with Bamazon!");
-                    start();
-                });
-
-
-            } else {
-                console.log("uh-oh! We don't seem to have enough inventory in stock, sorry!");
-                start();
+        inquirer.prompt([{
+            type: "input",
+            name: "itemID",
+            message: "What is the ID of the product you would like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) == false && parseInt(value) <= response.length && parseInt(value) > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
+        }, {
+            type: "input",
+            name: "quantity",
+            message: "What quantity would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }]).then(function(answer) {
 
+            var purchasedItem = (answer.itemID);
+            var numberToBuy = parseInt(answer.quantity);
+            for (var i = 0; i < response.length; i++) {
+                if (response[i].item_id == purchasedItem) {
+                    if (response[i].stock_quantity >= numberToBuy) {                        
+                        connection.query("UPDATE Products SET ? WHERE ?", [{
+                            stock_quantity: (response[i].stock_quantity - numberToBuy)
+                        }, {
+                            item_id: purchasedItem
+                        }], function(err, result) {
+                            if (err) throw err;                            
+                            var purchaseTotal = parseFloat(((response[i].price) * numberToBuy).toFixed(2));
+                            console.log("Success! Your total is $" + purchaseTotal.toFixed(2) + ". Thanks for your business with Bamazon!");
+                            buyItem();
+                        });
+                    } else {
+                        console.log("uh-oh! We don't seem to have enough inventory in stock, sorry!");
+                        buyItem();
+                    }
+                    break;
+                }
+            }
         })
     })
 }
